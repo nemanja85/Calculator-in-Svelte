c2cc34288cd80ba5504099b592b1cd53f7ceba42
nemanja85 <mitic@deutsche-teilkauf.de>
2022-07-26 20:27:43 +0200

    Added display and operations in calculator

---
+++ src/lib/Calculation.svelte
@@ -0,0 +1,30 @@
+<script>
+  let total = 0;
+  let state = null;
+  let console = '';
+
+  function setOperation(operation){
+
+  }
+</script>
+
+<div class="calculator">
+  <input type="text" bind:value={console} />
+  <div class="buttons">
+    <div class="operations">
+        <button on:click={() =>{setOperation('add');}}>
+        +
+      </button>
+      <button on:click={() =>{setOperation('substract');}}>
+        -
+      </button>
+      <button on:click={() =>{setOperation('multiply');}}>
+        *
+      </button>
+      <button on:click={() =>{setOperation('divide');}}>
+        /
+      </button>
+    </div>
+  </div>
+</div>
+
