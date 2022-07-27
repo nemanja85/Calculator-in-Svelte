d1b74be0998fa8d9949fe21bf920aee144df10ba
nemanja85 <mitic@deutsche-teilkauf.de>
2022-07-27 21:37:01 +0200

    Resolved conflict in calculation component

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,17 @@
 </script>
 
 <main>
+<<<<<<< Updated upstream
     <Calculation />
+||||||| Stash base
+  <h1>Vite + Svelte</h1>
+  <div class="card">
+    <Calculation />
+  </div>
+=======
+  <h1>Vite + Svelte</h1>
+  <Calculation />
+>>>>>>> Stashed changes
 </main>
 
 <style>
