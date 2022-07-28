cc18e4d81a30323eb57a444aee4b08a7ef8087c8
nemanja85 <mitic@deutsche-teilkauf.de>
2022-07-28 17:42:33 +0200

    Add style for calculator

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,7 @@
 </script>
 
 <main>
-<<<<<<< Updated upstream
     <Calculation />
-||||||| Stash base
-  <h1>Vite + Svelte</h1>
-  <div class="card">
-    <Calculation />
-  </div>
-=======
-  <h1>Vite + Svelte</h1>
-  <Calculation />
->>>>>>> Stashed changes
 </main>
 
 <style>
